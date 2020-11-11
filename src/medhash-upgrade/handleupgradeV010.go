3e7dca96a9c302b3b16e62d0d6f53aee74f8d317
GHIFARI160 <ghifari@ghifari160.com>
2020-11-11 03:10:27 -0600

    Implement medhash-upgrade
    - Implement upgrade handler for medhash v0.1.0 (legacy)
    - Implement dummy upgrade handler for medhash v0.2.0 (current)
    - Implement medhash-upgrade

---
+++ src/medhash-upgrade/handleupgradeV010.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/ghifari160/medhash-tools/src/data"
+	"github.com/ghifari160/medhash-tools/src/packageinfo"
+)
+
+func handleUpgradeV010(root string) error {
+	legacyMedhashFile, err := ioutil.ReadFile(root + "/sums.txt")
+
+	if err != nil {
+		return err
+	}
+
+	legacyMedhash := string(legacyMedhashFile)
+	legacyMedhashMedia := strings.Split(legacyMedhash, "\n")
+
+	var media []data.Media
+
+	fmt.Println("Upgrading legacy medhash file")
+
+	errCount := 0
+	for i := 0; i < len(legacyMedhashMedia); i++ {
+		legacyMedia := strings.Split(legacyMedhashMedia[i], "  ")
+
+		if len(legacyMedia) > 1 {
+			legacyPath := legacyMedia[1]
+			legacyHash := legacyMedia[0]
+
+			if len(legacyPath) > 2 && legacyPath[0:2] == "./" {
+				legacyPath = legacyPath[2:]
+			}
+
+			compPath := legacyPath
+
+			if root != "." {
+				compPath = root + "/" + legacyPath
+			}
+
+			fmt.Print("  ")
+			fmt.Print(compPath)
+			fmt.Print(": ")
+
+			var hash data.Hash
+			hash, err = data.GenHash(compPath)
+
+			if err != nil {
+				errCount++
+				fmt.Println("Error")
+			} else {
+				if hash.SHA256 != legacyHash {
+					errCount++
+					fmt.Println("Error")
+				} else {
+					fmt.Println("OK")
+
+					media = append(media,
+						data.Media{
+							Path: legacyPath,
+							Hash: hash,
+						})
+				}
+			}
+		}
+	}
+
+	if errCount > 0 {
+		fmt.Fprintln(os.Stderr, "Error: Media integrity error detected!")
+		return errors.New("media integrity")
+	}
+
+	fmt.Println("Sanity checking upgraded medhash")
+
+	errCount = 0
+	for i := 0; i < len(media); i++ {
+		berr := false
+
+		compPath := media[i].Path
+
+		if root != "." {
+			compPath = root + "/" + compPath
+		}
+
+		fmt.Print("  ")
+		fmt.Print(compPath)
+		fmt.Print(": ")
+
+		berr = !data.ChkHash(compPath, media[i].Hash)
+
+		if berr {
+			errCount++
+			fmt.Println("Error")
+		} else {
+			fmt.Println("OK")
+		}
+	}
+
+	if errCount > 0 {
+		fmt.Fprintln(os.Stderr, "Error: Media integrity error detected!")
+		return errors.New("media integrity")
+	}
+
+	medhash := data.Medhash{
+		Version: packageinfo.Version,
+		Media:   media,
+	}
+
+	medhashJSON, err := json.Marshal(medhash)
+	if err != nil {
+		return err
+	}
+
+	medhashFile, err := os.Create(root + "/medhash.json")
+	if err != nil {
+		return err
+	}
+
+	_, err = medhashFile.Write(medhashJSON)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ src/medhash-upgrade/handleupgradeV020.go
@@ -0,0 +1,7 @@
+package main
+
+import "errors"
+
+func handleUpgradeV020(root string) error {
+	return errors.New("medhash file version is current")
+}
