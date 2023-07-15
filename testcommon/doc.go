69df227611fd8b42bdb0516ae51f843aad535868
GHIFARI160 <ghifari@ghifari160.com>
2023-07-15 06:09:35 -0500

    Add package testcommon
    Package testcommon implements helper functions for use in tests.

---
+++ testcommon/doc.go
@@ -0,0 +1,6 @@
+// MedHash Tools
+// Copyright (c) 2023 GHIFARI160
+// MIT License
+
+// Package testcommon implements helper functions for use in tests.
+package testcommon
---
+++ testcommon/genpayload.go
@@ -0,0 +1,77 @@
+// MedHash Tools
+// Copyright (c) 2023 GHIFARI160
+// MIT License
+
+package testcommon
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/ghifari160/medhash-tools/medhash"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/sha3"
+)
+
+const MaxBuffer = 1 * 1024 * 1024 * 1024
+
+// GenPayload generates a test payload of size in dir.
+// The payload is wrapped around medhash.Media with all supported hash precalculated.
+func GenPayload(t testing.TB, dir string, size int64) (payload medhash.Media) {
+	var buf []byte
+	var counter int64
+
+	require := require.New(t)
+
+	t.Log("Generating payload")
+
+	if size > MaxBuffer {
+		buf = make([]byte, MaxBuffer)
+	} else {
+		buf = make([]byte, size)
+	}
+
+	payload.Path = "payload"
+
+	f, err := os.Create(filepath.Join(dir, payload.Path))
+	require.NoError(err)
+
+	sha3 := sha3.New256()
+	sha256 := sha256.New()
+	sha1 := sha1.New()
+	md5 := md5.New()
+	writer := io.MultiWriter(f, sha3, sha256, sha1, md5)
+
+	for counter < size {
+		n, err := rand.Read(buf)
+		require.NoError(err)
+
+		if counter+int64(n) > size {
+			n, err = writer.Write(buf[:size-counter])
+		} else {
+			n, err = writer.Write(buf)
+		}
+		require.NoError(err)
+
+		counter += int64(n)
+	}
+
+	err = f.Close()
+	require.NoError(err)
+
+	payload.Hash.SHA3_256 = hex.EncodeToString(sha3.Sum(nil))
+	payload.Hash.SHA256 = hex.EncodeToString(sha256.Sum(nil))
+	payload.Hash.SHA1 = hex.EncodeToString(sha1.Sum(nil))
+	payload.Hash.MD5 = hex.EncodeToString(md5.Sum(nil))
+
+	t.Log("Done generating payload")
+
+	return
+}
