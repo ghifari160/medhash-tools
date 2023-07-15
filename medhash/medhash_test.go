3e70118b3e77a24ab0b0d806f50dd1fca4bc2ff2
GHIFARI160 <ghifari@ghifari160.com>
2023-07-15 06:13:27 -0500

    Use testcommon.GenPayload in package medhash tests
    MedHashTestSuite.GenPayload implements the same functionality as
    testcommon.GenPayload, but with a different return type. Use testcommon
    internally.

--- medhash/medhash_test.go
+++ medhash/medhash_test.go
@@ -1,68 +1,23 @@
+// MedHash Tools
+// Copyright (c) 2023 GHIFARI160
+// MIT License
+
 package medhash_test
 
 import (
-	"crypto/rand"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/ghifari160/medhash-tools/medhash"
+	"github.com/ghifari160/medhash-tools/testcommon"
 	"github.com/stretchr/testify/suite"
 )
-
-const maxBuffer = 1 * 1024 * 1024 * 1024
 
 type MedHashTestSuite struct {
 	suite.Suite
 }
 
 func (m *MedHashTestSuite) GenPayload(t testing.TB, dir string, size int64) (payload string) {
-	var buf []byte
-	var counter int64
-
-	t.Log("Generating payload")
-
-	if size > maxBuffer {
-		buf = make([]byte, maxBuffer)
-	} else {
-		buf = make([]byte, size)
-	}
-
-	payload = "payload"
-
-	f, err := os.Create(filepath.Join(dir, payload))
-	if err != nil {
-		t.Fatalf("Cannot generate payload: %v", err)
-		return
-	}
-
-	for counter < size {
-		n, err := rand.Read(buf)
-		if err != nil {
-			t.Fatalf("Cannot generate payload: %v", err)
-			return
-		}
-
-		if counter+int64(n) > int64(size) {
-			n, err = f.Write(buf[:size-counter])
-		} else {
-			n, err = f.Write(buf)
-		}
-		if err != nil {
-			t.Fatalf("Cannot generate payload: %v", err)
-		}
-
-		counter += int64(n)
-	}
-
-	err = f.Close()
-	if err != nil {
-		t.Fatalf("Cannot generate payload: %v", err)
-	}
-
-	t.Log("Done generating payload")
-
-	return
+	return testcommon.GenPayload(t, dir, size).Path
 }
 
 func (s *MedHashTestSuite) TestNew() {
