da4bced131301368ede96bf3ce2e69131595351c
GHIFARI160 <ghifari@ghifari160.com>
2023-07-28 16:21:40 -0500

    Add missing test cases to medhash.GenHash
    Test cases for medhash.GenHash should use medhash Config presets
    wherever possible. Additionally, add the missing default test case.

--- medhash/genhash_test.go
+++ medhash/genhash_test.go
@@ -73,20 +73,26 @@ func (s *MedHashTestSuite) TestGenHash() {
 		s.NotEmpty(m.Hash.MD5)
 	})
 
-	s.Run("all", func() {
-		conf := medhash.Config{
-			Dir:  dir,
-			Path: payload,
-
-			SHA3:   true,
-			SHA256: true,
-			SHA1:   true,
-			MD5:    true,
-		}
+	s.Run("default", func() {
+		conf := medhash.DefaultConfig
+		conf.Dir = dir
+		conf.Path = payload
 
 		m, err := medhash.GenHash(conf)
 		s.Require().NoError(err)
 
+		s.NotEmpty(m.Hash.XXH3)
+	})
+
+	s.Run("all", func() {
+		conf := medhash.AllConfig
+		conf.Dir = dir
+		conf.Path = payload
+
+		m, err := medhash.GenHash(conf)
+		s.Require().NoError(err)
+
+		s.NotEmpty(m.Hash.XXH3)
 		s.NotEmpty(m.Hash.SHA3)
 		s.NotEmpty(m.Hash.SHA3_256)
 		s.NotEmpty(m.Hash.SHA256)
