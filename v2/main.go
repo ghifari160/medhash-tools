498f23d9f5b7a1b9d85f1a56fa6fdb3c400ce8e9
GHIFARI160 <ghifari@ghifari160.com>
2025-09-17 00:32:10 -0500

    Update main to work with cmd/v2
    Package medhash/v2 rewrites the MedHash library. Package cmd/v2 is an
    updated commands package that works with this new version of the
    library. Update main to work with cmd/v2.
    
    Note: this update is versioned under v2 for now, but we will eventually
    replace the root main in a backwards incompatible way. This will be done
    prior to merging to main.

---
+++ v2/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alexflint/go-arg"
+	"github.com/ghifari160/medhash-tools/cmd/v2"
+)
+
+const Name = "MedHash Tools"
+const Version = "0.7.0"
+
+func main() {
+	var args struct {
+		Gen     *cmd.Gen        `arg:"subcommand:gen" help:"generate MedHash Manifest"`
+		Chk     *cmd.Chk        `arg:"subcommand:chk" help:"verify directories or files"`
+		Upgrade *cmd.Upgrade    `arg:"subcommand:upgrade" help:"upgrade MedHash Manifest"`
+		Ver     *cmd.GenericCmd `arg:"subcommand:version" help:"print tool version"`
+	}
+
+	p := arg.MustParse(&args)
+
+	if p.Subcommand() == nil {
+		p.WriteUsage(os.Stdout)
+		os.Exit(1)
+	}
+
+	printHeader()
+
+	var c cmd.Command
+
+	switch {
+	case args.Gen != nil:
+		c = args.Gen
+
+	case args.Chk != nil:
+		c = args.Chk
+
+	case args.Upgrade != nil:
+		c = args.Upgrade
+
+	case args.Ver != nil:
+		c = new(cmd.GenericCmd)
+	}
+
+	os.Exit(c.Execute())
+}
+
+func printHeader() {
+	fmt.Printf("%s v%s\n", Name, Version)
+}
